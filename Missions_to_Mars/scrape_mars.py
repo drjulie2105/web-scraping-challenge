761c8e9306a9d865f62ab6acbac9068e142feb6d
Julie Pyle <drjuliepyle@gmail.com>
2020-10-18 12:53:03 -0400

    Updated python file w/ mars table df and to_html code

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -23,98 +23,72 @@ def scrape ():
     mars_info = {}
 
 # In[3]:
+    mars_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    browser.visit(mars_url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    news_title = soup.find_all('div', class_='content_title')[0].text
+    news_p=soup.find_all('div', class_='article_teaser_body')[0].text
 
-
-mars_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
-browser.visit(mars_url)
-
-
-# In[4]:
-
-
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[7]:
-
-
-news_title = soup.find_all('div', class_='content_title')[0].text
-news_p=soup.find_all('div', class_='article_teaser_body')[0].text
-
-print(news_title)
-print(news_p)
 
 
 # In[17]:
 
-
-image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-nasa_url= 'https://www.jpl.nasa.gov'
-browser.visit(image_url)
-
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[18]:
-
-
-featured_image=soup.find_all('img')[3]["src"]
-featured_image_url = nasa_url + featured_image
-print(featured_image_url)
-
-
-# In[19]:
-
-
-mars_facts_url='https://space-facts.com/mars/'
-browser.visit(mars_facts_url)
-html =browser.html
-soup=BeautifulSoup (html, 'html.parser')
-
-
-# In[20]:
-
-
-tables = pd.read_html(mars_facts_url)
-tables
-
-
-# In[30]:
-
-
-hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(hemisphere_url)
-
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# In[31]:
-
-
-hemisphere_image_urls=[]
-products=soup.find ('div', class_='result-list')
-hemispheres=products.find_all('div',{'class':'item'})
-
-for hemisphere in hemispheres:
-    title = hemisphere.find("h3").text
-    title = title.replace("Enhanced", "")
-    image_link = hemisphere.find("a")["href"]
-    hemisphere_link = "https://astrogeology.usgs.gov/" + image_link    
-    browser.visit(hemisphere_link)
+    image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    nasa_url= 'https://www.jpl.nasa.gov'
+    browser.visit(image_url)
     html = browser.html
-    soup=BeautifulSoup(html, "html.parser")
-    downloads = soup.find("div", class_="downloads")
-    image_url = downloads.find("a")["href"]
-    hemisphere_image_urls.append({"title": title, "img_url": image_url})
+    soup = BeautifulSoup(html, 'html.parser')
+    featured_image=soup.find_all('img')[3]["src"]
+    featured_image_url = nasa_url + featured_image
+
+
+    mars_facts_url='https://space-facts.com/mars/'
+    browser.visit(mars_facts_url)
+    html =browser.html
+    soup=BeautifulSoup (html, 'html.parser')
+    tables = pd.read_html(mars_facts_url)
+    mars_facts_df = tables[2]
+    mars_facts_df.columns = ["Description", "Value"]
+    mars_facts_df
+    mars_html_table = mars_facts_df.to_html()
+    mars_html_table
+    mars_html_table.replace('\n', '')
+
+
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemisphere_url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    hemisphere_image_urls=[]
+    products=soup.find ('div', class_='result-list')
+    hemispheres=products.find_all('div',{'class':'item'})
+
+    for hemisphere in hemispheres:
+        title = hemisphere.find("h3").text
+        title = title.replace("Enhanced", "")
+        image_link = hemisphere.find("a")["href"]
+        hemisphere_link = "https://astrogeology.usgs.gov/" + image_link    
+        browser.visit(hemisphere_link)
+        html = browser.html
+        soup=BeautifulSoup(html, "html.parser")
+        downloads = soup.find("div", class_="downloads")
+        image_url = downloads.find("a")["href"]
+        hemisphere_image_urls.append({"title": title, "img_url": image_url})
 
 
 # In[32]:
+    mars_dict = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "featured_image_url": featured_image_url,
+        "mars_weather": mars_weather,
+        "fact_table": str(mars_html_table),
+        "hemisphere_images": hemisphere_image_urls
+    }
 
+    return mars_dict
 
-hemisphere_image_urls
 
 
 # In[ ]:
